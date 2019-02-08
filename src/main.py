f7cd29033cf19b0cd248f1ac8f7269f21e5253a9
Donal Mee <mee.donal@gmail.com>
2019-02-08 23:46:10 +0000

    Outline of line tracer

---
+++ src/main.py
@@ -0,0 +1,90 @@
+import argparse
+import sys
+from importlib import import_module
+
+
+GLOBAL_LIST = []
+
+
+def log_line(frame):
+    info = {'event': 'line', 'line_no': frame.f_lineno,
+    'scope': frame.f_code.co_name, 'vars': frame.f_code.}
+    GLOBAL_LIST.append(info)
+
+
+def log_return(frame, arg):
+    info = {'event': 'return', 'line_no': frame.f_lineno,
+    'scope': frame.f_code.co_name, 'vars': frame.f_code.,
+    'return_value': arg}
+    GLOBAL_LIST.append(info)
+
+
+def log_call(frame, arg):
+    info = {'event': 'call', 'line_no': frame.f_lineno,
+    'scope': frame.f_code.co_name, 'vars': frame.f_code.,
+    'call_value': arg}
+    GLOBAL_LIST.append(info)
+
+
+def log_exception(frame, arg):
+    info = {'event': 'exception', 'line_no': frame.f_lineno,
+    'scope': frame.f_code.co_name, 'vars': frame.f_code.,
+    'exception_value': arg}
+    GLOBAL_LIST.append(info)
+
+
+class Tracer(object):
+    def __init__(self, target):
+        """target is module name we want to trace, other py modules will
+        be excluded from the tracing"""
+        self.target = target + ".py"
+
+    def hook(self, frame, event, arg):
+        # self.target is just the last part of the path
+        # whereas co_filename is probably the whole path
+        if self.target in frame.f_code.co_filename:
+            # Good, we are in the module we want to trace
+            # Log the line we are at and return the tracer
+            log_line(frame)
+            return self.tracer
+        else:
+            # Else don't hook this, do nothing
+            return None
+
+    def tracer(self, frame, event, arg):
+        if self.target not in frame.f_code.co_filename:
+            return None # if we aren't the target file do nothing
+        else:
+            if event == 'line':
+                log_line(frame)
+            elif event == 'call':
+                log_call(frame, arg)
+            elif event == 'return':
+                log_return(frame, arg)
+            elif event == 'exception':
+                # Note it is unhandled exceptions we care about more
+                # than normal exceptions. We haven't detected whether 
+                # it is unhandled yet, nor have we written a wrapper
+                # for catching them.
+                log_exception(frame, arg)
+            else:
+                # Nothing else is interesting, ignoring c functions atm
+                return None
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("module_to_import")
+    args = parser.parse_args()
+    tracer = Tracer(args.module_to_import)
+    # hack on the path...
+    sys.path.append("C:\\Users\\meedo\\code\\flightbox\\test")
+    sys.settrace(tracer.hook)
+    import_module(args.module_to_import)
+    for x in GLOBAL_LIST:
+        print x
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/example1.py
@@ -0,0 +1,21 @@
+import glob  # just import a random module, do not log in glob
+
+def hello_name(name):
+    print "hello %s" % name # @log line 4, value of name
+
+
+def add(x, y):
+    result = x + y  # log @line 10, x, y and result values
+    return result # log @line 9, x, y and result values
+
+hello_name("donal") # log @line 11,
+add(3, 2)
+
+'''So I expect it to log import glob, but not log anything from
+the glob module.
+Then it will log it loads hello_name at line 3, loads add at line 7
+Then it will log it's at line 11
+Then line 3, 4, 4 back to 11
+Then line 12, to 7, 8 and 9, 9 to 12
+Then it may log this comment section, not sure.
+'''
